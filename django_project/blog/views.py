3bf0bfc4192906c9b8d6c20843660df3f3411b21
Jan Son Ha <hathanhson2706@gmail.com>
2020-12-19 21:52:44 +0100

    posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Jan Son Ha',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'data_posted': 'August 19, 2020'
+    },
+    {
+        'author': 'Maciek',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'data_posted': 'August 19, 2020'
+    }
+]
 
 # Create your views here.
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
