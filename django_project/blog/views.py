9e291dea55ddea39b99df3f98a046e6e0d7c6624
Jan Son Ha <hathanhson2706@gmail.com>
2020-12-19 21:06:17 +0100

    Update

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
 # Create your views here.
+def home(request):
+    return render(request, 'blog/home.html')
+
+
+def about(request):
+    return render(request, 'blog/about.html')
