99bf2b068d617f95f79056a68d80e1a986fe06e7
Jan Son Ha <hathanhson2706@gmail.com>
2020-12-20 23:51:10 +0100

    added register app

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Jan Son Ha',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 19, 2020'
-    },
-    {
-        'author': 'Maciek',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 19, 2020'
-    }
-]
+# from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
