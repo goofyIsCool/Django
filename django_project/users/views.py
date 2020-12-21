8d2fbbbd7c2f94ee784064890b0858713cebf54f
Jan Son Ha <hathanhson2706@gmail.com>
2020-12-21 14:26:23 +0100

    Update

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             # password = form.cleaned_data.get('password')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(
+                request, f'Your account has been created {username}! You are now able to login.')
+            return redirect('login')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
